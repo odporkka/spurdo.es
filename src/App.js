8f527dd9891b0fbaf3961371c15c3a7921d9e0b1
Otto Porkka <otto.porkka@iki.fi>
2019-12-30 23:59:59 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './es.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Ebin!! :-Dddd</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
